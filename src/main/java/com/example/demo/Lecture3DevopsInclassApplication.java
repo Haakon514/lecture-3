fa3646cd176018ab133d280e67cea990ebfd7ef2
EC2 Default User <ec2-user@ip-172-31-32-43.eu-west-1.compute.internal>
2024-09-14 17:42:12 +0000

    first commit

---
+++ src/main/java/com/example/demo/Lecture3DevopsInclassApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecture3DevopsInclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lecture3DevopsInclassApplication.class, args);
+	}
+
+}
