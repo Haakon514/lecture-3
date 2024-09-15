b48a7cffa0f7c5e8002ed3b88292509b075bb280
EC2 Default User <ec2-user@ip-172-31-32-43.eu-west-1.compute.internal>
2024-09-15 11:57:03 +0000

    should build now

--- src/main/java/com/example/demo/Lecture3TryApplication.java
+++ src/main/java/com/example/demo/Lecture3TryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lecture3DevopsInclassApplication {
+public class Lecture3TryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lecture3DevopsInclassApplication.class, args);
+		SpringApplication.run(Lecture3TryApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/demo/Lecture3TryApplicationTests.java
+++ src/test/java/com/example/demo/Lecture3TryApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Lecture3DevopsInclassApplicationTests {
+class Lecture3TryApplicationTests {
 
 	@Test
 	void contextLoads() {
